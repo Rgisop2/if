47ebc5464ea6047f60adeef8cd71dd1fe1a38079
Rgisop2 <160823869+Rgisop2@users.noreply.github.com>
2025-11-08 00:09:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", "28079855"))
-API_HASH = environ.get("API_HASH", "3493b73fe86e88f409ac7b95d23cff5b")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8421516268:AAHX-hFv4zLEWv1J49bQGOD3JyA3xEScMAg")
+API_ID = int(environ.get("API_ID", "27353035"))
+API_HASH = environ.get("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8298659033:AAEjD8TQsX7q3KTn-4xAwPxfcyZ4Ck08wrA")
 
 # Make Bot Admin In Log Channel With Full Rights
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002073865889"))
 ADMINS = int(environ.get("ADMINS", "1327021082"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "mongodb+srv://url4xcom:hello@cluster0.5ldewyx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = environ.get("DB_URI", "mongodb+srv://Rgisop09:Rgisop09@cluster0.lo6sg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = environ.get("DB_NAME", "vjlinkchangerbot")
